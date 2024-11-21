8985c67e61d0497b49c5312bdf328d01733b81a0
m-abdullahusmani <106439064+m-abdullahusmani@users.noreply.github.com>
2024-11-21 22:57:28 +0500

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,66 @@
+var products = [
+    {
+        "product_name": "Short sleeve Tee shirts are the perfect gift for Birthdays, Holidays, or any Special Event.",
+        "price": 800,
+        "image": "https://m.media-amazon.com/images/I/A1ntnF3PJOL._CLa%7C2140%2C2000%7C71cw8KU3MEL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX466_.png"
+    },
+    {
+        "product_name": "Are you the queen of thrift store shopping? Here's the perfect t-shirt for thrifting.",
+        "price": 680,
+        "image": "https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C61lynEYDdfL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX466_.png"
+    },
+    {
+        "product_name": "Sleeve Length:Short Style:Casual " ,
+        
+        "price": 500,
+        "image": "https://img.joomcdn.net/723025a94e2f81b3d497d8c8980848b2940c8ee0_1007_1024.jpeg"
+    },
+    {
+        "product_name": "Vintage Fashion illustration George Barbier Costumes ssential T-Shirt",
+        "price": 1999,
+        "image": "https://ih1.redbubble.net/image.3324426658.5422/gptr,1400x,front,black-c,188,133,1000,1000-bg,f8f8f8.jpg"
+    },
+    {
+        "product_name": "Short sleeve Tee shirts are the perfect gift for Birthdays, Holidays, or any Special Event.",
+        "price": 900,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhLKoVOLrzq4SM5aEF1j4nwaS1uQy0sic4Qg&usqp=CAU"
+    },
+    {
+        "product_name": "Are you the queen of thrift store shopping? Here's the perfect t-shirt for thrifting.",
+        "price": 850,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9g57B8OjlPyflif1oP-hZRH5yqi1m4UChBg&usqp=CAU"
+    },
+    {
+        "product_name": "Sleeve Length:Short Style:Casual " ,
+        
+        "price": 500,
+        "image": "https://m.media-amazon.com/images/I/71Um-Cv--CL._AC_UX522_.jpg"
+    },
+    {
+        "product_name": "Vintage Fashion illustration George Barbier Costumes ssential T-Shirt",
+        "price": 1999,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVpQHwbzdVqwjAZdWEQP3AxlPZ8ajfbvr45AgoD8LocOFmyvwnH1IIM-jmTNVWVdkkbxg&usqp=CAU."
+    },
+    {
+        "product_name": "Sleeve Length:Short Style:Casual " ,
+        
+        "price": 500,
+        "image": "https://m.media-amazon.com/images/I/81z15oqB3DL._AC_UX569_.jpg"
+    },
+    {
+        "product_name": "Vintage Fashion illustration George Barbier Costumes ssential T-Shirt",
+        "price": 999,
+        "image": "https://m.media-amazon.com/images/I/815NbVTdp1L._AC_UX569_.jpg"
+    },
+    {
+        "product_name": "Sleeve Length:Short Style:Casual " ,
+        
+        "price": 500,
+        "image": "https://m.media-amazon.com/images/I/B1JoQqb1C2S._CLa%7C2140%2C2000%7C91bzMrU66VL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX466_.png"
+    },
+    {
+        "product_name": "Vintage Fashion illustration George Barbier Costumes ssential T-Shirt",
+        "price": 1999,
+        "image": "https://m.media-amazon.com/images/I/71RgvEB9shS._AC_UX569_.jpg"
+    },
+];
